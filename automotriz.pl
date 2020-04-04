c089782b3ac9d1e96961bc32287384bdf4d24576
LuisJo Ruano <rp.luisjose@gmail.com>
2020-04-03 20:48:30 -0600

    ADD: string modified

--- automotriz.pl
+++ automotriz.pl
@@ -7,7 +7,7 @@
 inicio:-
 %   Declaración
 	new(Menu, dialog('Diagnostico Automotriz (Main Menu)', size(1000,1000))),
-	new(L,label(nombre,'DA-LJRP v1.00.09')),
+	new(L,label(nombre,'DA-UMG-LJRP v1.00.09')),
 	new(@texto,label(nombre,'Realiza un nuevo diagnóstico para continuar')),
 	new(@boton,button('realizar diagnóstico',message(@prolog,botones))),
 	new(A,label(nombre,'Luis José Ruano Pérez 2690-12-4853')),
@@ -28,10 +28,10 @@ inicio:-
 %   Soluciones a las fallas de acuerdo a los resultados del diagnóstico
 
 fallas('Cambio de Suspesión:
-        El cambio de suspesión debe ser realizado en un taller,
-        en el cual se deberá realizar el chequeo respectivo de
-        amortiguadores.
-        Esto es la razón también del desgaste anormal de llantas.'):-suspension,!.
+    El cambio de suspesión debe ser realizado en un taller,
+    en el cual se deberá realizar el chequeo respectivo de
+    amortiguadores.
+    Esto es la razón también del desgaste anormal de llantas.'):-suspension,!.
 
 fallas('Cambio de Aceite:
 	1) Enciende el carro durante 5 o 10 minutos y luego apágalo.
@@ -50,35 +50,27 @@ fallas('verificar el estado actual de la bateria:
         verifique si estan bien conctados los cables, arranque
 	el coche, si no arranca entonces la bateria esta muerta
 	para esto recarguela pase corriente con otro coche,
-	en caso de no tener exito debera reemplazar la bateria'):-electronico,!.
+	en caso de no tener exito debera reemplazar la bateria'):-electrico,!.
 
-fallas('llego la hora de cambiar tus pastillas de freno:
-	si se escucha un chillido agudo al frenar es tiempo
-        de cambiar las pastillas de los frenos, para ello hay
-	que levantar con un gato hidraulico el lado del freno
-	donde se va a cambiar, con una llave inglesa y una
-	matraca aflojar los cubre pastillas y sacar las patillas
-	antiguas y reponerlas con las nuevas, colocar todo en su
-	lugar y bla bla bla. '):-frenos,!.
+fallas('Cambio en las pastillas de freno.
+	El chillido que se escucha al frenar es representación que las
+	pastillas de freno ya estan muy gastadas y podrían rayar el
+	disco del freno.
+	Para cambiarlas hay que realizar lo siguiente:
+	1) Levantar con un gato hidraulico el lado del freno donde se va a cambiar.
+	2) Con una llave inglesa y una llave trinquete para aflojar los cubre pastillas
+	y sacar las patillas antiguas y reponerlas con las nuevas, colocar todo en su
+	lugar. '):-frenos,!.
 
-fallas('posiblemente tu auto pasara a mejor vida:
-	esta luz puede indicar varias fallas en el sistema de la ECU,
-	las pricipales son fallas de sensores, servicio de motor,
-	catalizador, etc. si se cuenta con un escaner automotriz puede
-	borrarse la falla pero esto no arregla el problema, para ello
-	acuda con su mecanico certificado por los aliens.'):-computadora,!.
+fallas('Al parecer el alternador no pasa más el chispazo para el arranque del carro,
+    es necesario cuanto antes sustituirlo con un especialista.
+    Para realizar las pruebas de voltaje recuerda utilizar guantes de goma como aislante de electricidad.'):-alternador,!.
 
-fallas('seguro subes demaciado el volumen:
-	primero debes ubicar la bocina que no se escucha despues
-        quitar o desatornillar el caparcete que protege la bocina
-	y verificar que la bocina este bien conectado o tenga un cable
-	quemado, dado uno de los casos deberas cambiar el cable
-	o remplazar la bocina. Otro caso es verificar el estereo
-	del auto si estan bien conectados los cables'):-sonido,!.
+fallas('Si notas que el radiador de tu carro presenta una notable diferencia térmica, }
+    o si el motor de tu carro se está sobrecalentando, es necesario llevarlo a un taller de radiadores
+    cuanto antes, ya que puede llegar a fundir el motor.'):-fuga_radiador,!.
 
-
-fallas('sin resultados! si los problemas persisten utilice un dispositivo
-	alienigena con mas ram y 12 nucleos cpu:/').
+fallas('Sin resultados, para el diagnóstico. :(').
 
 % Preguntas para resolver los posibles problemas o fallas.
 % falla
@@ -90,11 +82,6 @@ suspension:- revision_suspension,
 	pregunta('¿Existe un desgaste anormal de las llantas? '),
 	pregunta('¿El volante se mueve bastante y tiembla?').
 
-frenos:- cambio_frenos,
-	pregunta('¿Tiene problemas con sus frenos?'),
-	pregunta('Cuando se frena ¿El carro hace un chillido?'),
-	pregunta('¿Al frenar el carro demora en desacelerar? ').
-
 aceite:- cambio_aceite,
 	pregunta('¿Problemas de motor?'),
 	pregunta('¿Su carro gasta más gasolina de lo debido?'),
@@ -102,46 +89,54 @@ aceite:- cambio_aceite,
 	pregunta('¿Su motor se escucha muy ruidoso? '),
 	pregunta('¿Tiene problemas para arrancar el carro en frio?').
 
-electronico:- bateria_agotada,
+electrico:- cambio_bateria,
+	pregunta('¿La bocina del carro suena baja o no suena?'),
 	pregunta('¿Problemas eléctricos?'),
+	pregunta('¿El carro hace un sonido crackeante al intentar encenderlo?'),
 	pregunta('¿Los silvines alumbran con poca fuerza?'),
-	pregunta('¿El tablero no enciende?'),
-	pregunta('¿El carro hace un sonido crackeante cuando enciende?'),
-	pregunta('¿El auto no hace ningun tipo de sonido al girar la llave?'),
-	pregunta('¿La bocina del carro suena baja o no suena?').
+	pregunta('¿El carro no hace ningun tipo de sonido al girar la llave?'),
+	pregunta('¿El tablero no enciende?').
 
+frenos:- cambio_frenos,
+	pregunta('¿Tiene problemas con sus frenos?'),
+	pregunta('Cuando se frena ¿El carro hace un chillido?'),
+	pregunta('¿Al frenar el carro demora en desacelerar? ').
 
-computadora:- check_egine,
-	pregunta('la luz check egine se encendio en tu tablero?'),
-	pregunta('la luz se mantiene encendida todo el tiempo?').
+alternador:- alternador_malo,
+	pregunta('¿La bocina del carro suena baja o no suena?'),
+	pregunta('¿Problemas eléctricos?'),
+	pregunta('¿El carro hace un sonido crackeante al intentar encenderlo?'),
+	pregunta('¿Los silvines alumbran con poca fuerza?'),
+	pregunta('¿El carro no hace ningun tipo de sonido al girar la llave?'),
+	pregunta('¿La batería presenta la capacidad correcta (12 voltios)?').
 
-sonido:- cambio_bocina,
-	pregunta('tienes problemas con alguna bocina?'),
-	pregunta('la bocina no se escucha nada?'),
-	pregunta('tu auto tiene suficiente bateria?').
+fuga_radiador:- radiador,
+	pregunta('¿El carro calienta con frecuencia?'),
+	pregunta('¿Existe fuga de vapor saliendo del capó o de la parte frontal del carro?'),
+	pregunta('¿Existe fuga de agua debajo del carro?').
 
 %identificador de falla que dirige a las preguntas correspondientes
 
 revision_suspension:-pregunta('¿Tiene problemas con sus frenos?'),!.
-cambio_frenos:-pregunta('¿Tiene problemas con sus frenos?'),!.
 cambio_aceite:-pregunta('¿Problemas de motor?'),!.
-bateria_agotada:-pregunta('¿Problemas electricos?'),!.
-cambio_bocina:-pregunta('tienes problemas con alguna bocina?'),!.
-check_egine:-pregunta('la luz check egine se encendio en tu tablero?'),!.
+cambio_bateria:-pregunta('¿La bocina del carro suena baja o no suena?'),!.
+cambio_frenos:-pregunta('¿Tiene problemas con sus frenos?'),!.
+alternador_malo:-pregunta('¿La batería presenta la capacidad correcta (12 voltios)?'),!.
+radiador:-pregunta('la luz check egine se encendio en tu tablero?'),!.
 
-% proceso del diagnostico basado en preguntas de si y no, cuando el
-% usuario dice si, se pasa a la siguiente pregunta del mismo ramo, si
-% dice que no se pasa a la pregunta del siguiente ramo
+% Proceso del diagnostico basado en preguntas de SI/NO.
+% Cuando el usuario dice SI, el parámetro que recibe es la siguiente pregunta de la misma falla.
+% Cuando dice NO, cambia a la primera pregunta de otra rama para evaluar la continuidad.
 % (motor,frenos,etc.)
 
-:-dynamic si/1,no/1.
-preguntar(Problema):- new(Di,dialog('Diagnostico mecanico')),
+:-dynamic si/1, no/1.
+preguntar(Problema):- new(Di,dialog('Diagnostico Automotríz AD-UMG-LJRP')),
      new(L2,label(texto,'Responde las siguientes preguntas')),
      new(La,label(prob,Problema)),
      new(B1,button(si,and(message(Di,return,si)))),
      new(B2,button(no,and(message(Di,return,no)))),
 
-         send(Di,append(L2)),
+     send(Di,append(L2)),
 	 send(Di,append(La)),
 	 send(Di,append(B1)),
 	 send(Di,append(B2)),
@@ -167,7 +162,7 @@ botones :- lim,
 	send(@boton,free),
 	send(@btncarrera,free),
 	fallas(Falla),
-	send(@texto,selection('la solucion es ')),
+	send(@texto,selection('Diagnóstico finalizado, resultado: ')),
 	send(@respl,selection(Falla)),
 	new(@boton,button('inicia procedimiento mecanico',message(@prolog,botones))),
         send(Menu,display,@boton,point(40,50)),
